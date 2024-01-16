0f1d84449007be8b6e6b56d3d11af2ff797746d9
Abhishek Kumar <abhishekkumar@Abhisheks-MacBook-Air-2.local>
2024-01-17 00:24:38 +0530

    Test update of Index.html

--- controllers/servicesController.js
+++ controllers/servicesController.js
@@ -2,8 +2,8 @@ const  services = require("../models/service-schema");
 
 const service = async (req,res) => {
     try{
-        const data = await services.find();
-        res.status(201).json({data});
+        // const data = await services.find();
+        res.status(201).json({"Message":"hi"});
     } catch(error){
         console.log(error);
     }
